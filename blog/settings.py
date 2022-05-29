7294cf82f4daad7faf4dd60c88b648d4e3efc864
calebcheptumo <ckibusia@gmail.com>
2022-05-29 03:23:11 -0400

    created API Views

--- blog/settings.py
+++ blog/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
+    
+
+    #third-party apps
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from urllib import response
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(http_method_names=["GET"])
+def homepage(request: Request):
+    response = {"message": "Hello world"}
+    return Response(data=response, status=status.HTTP_200_OK)
